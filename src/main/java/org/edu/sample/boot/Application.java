acc2bfca644b9243ff91b559103480489de3d9b3
Koby Aharon <kobyahron@gmail.com>
2018-05-19 14:42:53 +0300

    Adding spring-boot-actuator dependency

--- src/main/java/org/edu/sample/boot/Application.java
+++ src/main/java/org/edu/sample/boot/Application.java
@@ -3,6 +3,7 @@ package org.edu.sample.boot;
 import java.util.Arrays;
 
 import org.edu.sample.boot.config.BootWebappConfiguration;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,14 +13,21 @@ public class Application {
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-        
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+    }
+
+//    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
     }
 
 }
